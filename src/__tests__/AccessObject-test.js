2ddad10a66607757056242b5a63da60fc1800212
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-18 19:17:15 -0500

    use async/await instead of co

--- src/__tests__/AccessObject-test.js
+++ src/__tests__/AccessObject-test.js
@@ -14,13 +14,13 @@ describe('AccessObject', () => {
     ff.restore();
   });
 
-  it('should create new instances', co.wrap(function*() {
+  it('should create new instances', async () => {
     const data = { firstName: 'Jon', lastName: 'Snow' };
     const body = { id: '48fga412bca', firstName: 'Jon', lastName: 'Snow' };
 
     ff.mock('/resources/', 'POST', { status: 201, body });
 
-    const response = yield Resources.create(data);
+    const response = await Resources.create(data);
     const [path, request] = ff.calls().matched[0];
 
     const expected = { method: 'POST', body: data, headers: {},
@@ -29,15 +29,15 @@ describe('AccessObject', () => {
     assert.equal(path, '/resources/');
     assert.deepEqual(request, expected, 'Request is missing some fields');
     assert.deepEqual(response, body, 'Response should be a created entity');
-  }));
+  });
 
-  it('should retrieve full list', co.wrap(function*() {
+  it('should retrieve full list', async () => {
     // TODO: test for query
     const body = [{ id: '48fga412bca', firstName: 'Jon', lastName: 'Snow' }];
 
     ff.mock('/resources/', 'GET', { status: 200, body });
 
-    const response = yield Resources.retrieve();
+    const response = await Resources.retrieve();
     const [path, request] = ff.calls().matched[0];
 
     const expected = { method: 'GET', body: null, headers: {},
@@ -46,14 +46,14 @@ describe('AccessObject', () => {
     assert.equal(path, '/resources/');
     assert.deepEqual(request, expected);
     assert.deepEqual(response, body);
-  }));
+  });
 
-  it('should retrieve one instance', co.wrap(function*() {
+  it('should retrieve one instance', async () => {
     const body = { id: '48fga412bca', firstName: 'Jon', lastName: 'Snow' };
 
     ff.mock('/resources/48fga412bca', 'GET', { status: 200, body });
 
-    const response = yield Resources.retrieve('48fga412bca');
+    const response = await Resources.retrieve('48fga412bca');
     const [path, request] = ff.calls().matched[0];
 
     const expected = { method: 'GET', body: null, headers: {},
@@ -62,14 +62,14 @@ describe('AccessObject', () => {
     assert.equal(path, '/resources/48fga412bca');
     assert.deepEqual(request, expected);
     assert.deepEqual(response, body);
-  }));
+  });
 
-  it('should retrieve with query', co.wrap(function*() {
+  it('should retrieve with query', async () => {
     const body = [{ id: '48fga412bca', firstName: 'Jon', lastName: 'Snow' }];
 
     ff.mock(/\/resources\/\?.+/, 'GET', { status: 200, body });
 
-    const response = yield Resources.retrieve(null, { age: 23, name: 'Jon' });
+    const response = await Resources.retrieve(null, { age: 23, name: 'Jon' });
     const [path, request] = ff.calls().matched[0];
 
     const expected = { method: 'GET', body: null, headers: {},
@@ -78,14 +78,14 @@ describe('AccessObject', () => {
     assert.equal(path, '/resources/?age=23&name=Jon');
     assert.deepEqual(request, expected);
     assert.deepEqual(response, body);
-  }));
+  });
 
-  it('should retrieve one item with query', co.wrap(function*() {
+  it('should retrieve one item with query', async () => {
     const body = { id: '48fga412bca', firstName: 'Jon', lastName: 'Snow' };
 
     ff.mock(/\/resources\/48fga412bca\?.+/, 'GET', { status: 200, body });
 
-    const response = yield Resources.retrieve('48fga412bca', { age: 23,
+    const response = await Resources.retrieve('48fga412bca', { age: 23,
                                                                name: 'Jon' });
     const [path, request] = ff.calls().matched[0];
 
@@ -95,14 +95,14 @@ describe('AccessObject', () => {
     assert.equal(path, '/resources/48fga412bca?age=23&name=Jon');
     assert.deepEqual(request, expected);
     assert.deepEqual(response, body);
-  }));
+  });
 
-  it('should update instance', co.wrap(function*() {
+  it('should update instance', async () => {
     const body = { age: 23 };
 
     ff.mock('/resources/48fga412bca', 'PATCH', { status: 200, body });
 
-    const response = yield Resources.update('48fga412bca', body);
+    const response = await Resources.update('48fga412bca', body);
     const [path, request] = ff.calls().matched[0];
 
     const expected = { method: 'PATCH', body, headers: {}, credentials: null };
@@ -110,12 +110,12 @@ describe('AccessObject', () => {
     assert.equal(path, '/resources/48fga412bca');
     assert.deepEqual(request, expected);
     assert.deepEqual(response, body);
-  }));
+  });
 
-  it('should destroy instance', co.wrap(function*() {
+  it('should destroy instance', async () => {
     ff.mock('/resources/48fga412bca', 'DELETE', { status: 204 });
 
-    const response = yield Resources.destroy('48fga412bca');
+    const response = await Resources.destroy('48fga412bca');
     const [path, request] = ff.calls().matched[0];
 
     const expected = { method: 'DELETE', body: null, headers: {},
@@ -124,5 +124,5 @@ describe('AccessObject', () => {
     assert.equal(path, '/resources/48fga412bca');
     assert.deepEqual(request, expected);
     assert.deepEqual(response, null);
-  }));
+  });
 });
