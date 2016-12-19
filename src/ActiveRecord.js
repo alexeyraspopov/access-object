ab638bc0e6b6c550adfad4b5d32ce4b116fdaf11
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-18 19:29:52 -0500

    add main methods signature

--- src/ActiveRecord.js
+++ src/ActiveRecord.js
@@ -2,4 +2,24 @@
 export default class ActiveRecord {
 
 
+  /**
+   * @param {string} key — property name to fetch
+   */
+  get(key) { }
+
+  /**
+   * @param {string} key — property name to set
+   * @param {any} value — new value
+   */
+  set(key, value) { }
+
+  /**
+   * @return {Promise} — send current state of a record to its endpoint
+   */
+  save() { }
+
+  /**
+   * @return {Promise} — make DELETE request to record's endpoint
+   */
+  destroy() { }
 }
